cb9166569acf3eee7be6c43bd4e0fe382fe095de
ASZabavin <ASZabavin@DESKTOP-I4U134U>
2024-04-18 17:36:38 +1000

    fix

--- src/info_service/actions/win_initial_query_highlight_synonyms.py
+++ src/info_service/actions/win_initial_query_highlight_synonyms.py
@@ -15,29 +15,29 @@ import math
 
 
 RUS_POS = {
-    'S': 'существительное',
-    'A': 'прилагательное',
-    'NUM': 'числительное',
-    'A-NUM': 'численное прилагательное',
-    'V': 'глагол',
-    'ADV': 'наречие',
+    'S': 'сущ.',
+    'A': 'прил.',
+    'NUM': 'числ.',
+    'A-NUM': 'числ. прил.',
+    'V': 'глаг.',
+    'ADV': 'нар.',
     'PRAEDIC': 'предикатив',
     'PARENTH': 'вводное',
-    'S-PRO': 'местоим. сущ.',
-    'A-PRO': 'местоим. прил.',
-    'ADV-PRO': 'местоим. нареч.',
-    'PRAEDIC-PRO': 'местоим. предик.',
+    'S-PRO': 'мест. сущ.',
+    'A-PRO': 'мест. прил.',
+    'ADV-PRO': 'мест. нареч.',
+    'PRAEDIC-PRO': 'мест. предик.',
     'PR': 'предлог',
     'PART': 'частица',
     'INIT': 'инит',
     'NONLEX': 'нонлекс',
 
-    'DET': 'местоимение',
+    'DET': 'мест.',
     'CCONJ': 'союз',
 
     'ADP': 'предлог',
-    'NOUN': 'имя существительное',
-    'ADJ': 'имя прилагательное',
+    'NOUN': 'сущ.',
+    'ADJ': 'прил.',
     'ADJF': 'имя прилагательное (полное)',
     'ADJS': 'имя прилагательное (краткое)',
     'COMP': 'компаратив',
@@ -46,8 +46,8 @@ RUS_POS = {
     'PRTF': 'причастие (полное)',
     'PRTS': 'причастие (краткое)',
     'GRND': 'деепричастие',
-    'NUMR': 'числительное',
-    'ADVB': 'наречие',
+    'NUMR': 'числ.',
+    'ADVB': 'нар.',
     'NPRO': 'местоимение-существительное',
     'PRED': 'предикатив',
     'PREP': 'предлог',
@@ -58,8 +58,11 @@ RUS_POS = {
 }
 
 
-def get_has_in_postgres_TF_IDF(lexem):
+def get_has_in_postgres_TF_IDF(lexem, only_questions=False):
     session = Session()
+    que_type = 0
+    if only_questions:
+        que_type = 1
 
     sqlstr = '''
 
@@ -69,24 +72,40 @@ def get_has_in_postgres_TF_IDF(lexem):
         DECLARE
           answer RECORD;
         BEGIN
-          FOR answer IN SELECT id FROM telegram_bot_link_base
-          LOOP
-                RETURN QUERY select answer.id as ans_id, lexems.word, lexems.nentry num_in_doc, lexems.weight_norm, (select SUM(allexem.cnt)
-                from
-                (
-                SELECT count(*) cnt FROM ts_stat(''SELECT search_vector FROM telegram_bot_link_base where telegram_bot_link_base.id=''||answer.id::VARCHAR , ''ABCD'')
-                ) allexem) from
-                (
-                SELECT 1.0 as weight_norm,  * FROM ts_stat(''SELECT search_vector FROM telegram_bot_link_base where telegram_bot_link_base.id=''||answer.id::VARCHAR, ''A'')
-                union all
-                SELECT 0.4 as weight_norm, * FROM ts_stat(''SELECT search_vector FROM telegram_bot_link_base where telegram_bot_link_base.id=''||answer.id::VARCHAR, ''B'')
-                union all
-                SELECT 0.2 as weight_norm,  * FROM ts_stat(''SELECT search_vector FROM telegram_bot_link_base where telegram_bot_link_base.id=''||answer.id::VARCHAR, ''C'')
-                union all
-                SELECT 0.1 as weight_norm,  * FROM ts_stat(''SELECT search_vector FROM telegram_bot_link_base where telegram_bot_link_base.id=''||answer.id::VARCHAR, ''D'')
-                ) lexems;
-          END LOOP;
-          RETURN;
+            IF query_type = 0 THEN
+              FOR answer IN SELECT id FROM telegram_bot_link_base
+              LOOP
+                    RETURN QUERY select answer.id as ans_id, lexems.word, lexems.nentry num_in_doc, lexems.weight_norm, (select SUM(allexem.cnt)
+                    from
+                    (
+                    SELECT count(*) cnt FROM ts_stat(''SELECT search_vector FROM telegram_bot_link_base where telegram_bot_link_base.id=''||answer.id::VARCHAR , ''ABCD'')
+                    ) allexem) from
+                    (
+                    SELECT 1.0 as weight_norm,  * FROM ts_stat(''SELECT search_vector FROM telegram_bot_link_base where telegram_bot_link_base.id=''||answer.id::VARCHAR, ''A'')
+                    union all
+                    SELECT 0.4 as weight_norm, * FROM ts_stat(''SELECT search_vector FROM telegram_bot_link_base where telegram_bot_link_base.id=''||answer.id::VARCHAR, ''B'')
+                    union all
+                    SELECT 0.2 as weight_norm,  * FROM ts_stat(''SELECT search_vector FROM telegram_bot_link_base where telegram_bot_link_base.id=''||answer.id::VARCHAR, ''C'')
+                    union all
+                    SELECT 0.1 as weight_norm,  * FROM ts_stat(''SELECT search_vector FROM telegram_bot_link_base where telegram_bot_link_base.id=''||answer.id::VARCHAR, ''D'')
+                    ) lexems;
+              END LOOP;
+            ELSE
+              FOR answer IN SELECT id FROM telegram_bot_link_base
+              LOOP
+                    RETURN QUERY select answer.id as ans_id, lexems.word, lexems.nentry num_in_doc, lexems.weight_norm, (select SUM(allexem.cnt)
+                    from
+                    (
+                    SELECT count(*) cnt FROM ts_stat(''SELECT q_search_vector FROM telegram_bot_link_base where telegram_bot_link_base.id=''||answer.id::VARCHAR , ''AB'')
+                    ) allexem) from
+                    (
+                    SELECT 1.0 as weight_norm,  * FROM ts_stat(''SELECT q_search_vector FROM telegram_bot_link_base where telegram_bot_link_base.id=''||answer.id::VARCHAR, ''A'')
+                    union all
+                    SELECT 0.4 as weight_norm, * FROM ts_stat(''SELECT q_search_vector FROM telegram_bot_link_base where telegram_bot_link_base.id=''||answer.id::VARCHAR, ''B'')
+                    ) lexems;
+              END LOOP;
+            END IF;
+            RETURN;
         END;
         '
 
@@ -104,11 +123,11 @@ def get_has_in_postgres_TF_IDF(lexem):
     sqlstr = '''
 
         select * from (
-        SELECT * FROM public.telegram_bot_link_base_get_words(1) f(ans_id int, word text, num_in_doc int4, weight_norm numeric, summa numeric)
-        ) lemms where length(lemms.word)>1 and not isnumeric(lemms.word) and  lemms.word='{}'
+        SELECT * FROM public.telegram_bot_link_base_get_words({que_type}) f(ans_id int, word text, num_in_doc int4, weight_norm numeric, summa numeric)
+        ) lemms where length(lemms.word)>1 and not isnumeric(lemms.word) and  lemms.word='{lexem}'
         ORDER BY summa DESC, weight_norm DESC, word
 
-    '''.format(lexem)
+    '''.format(lexem=lexem, que_type=que_type)
 
     results = session.execute(sqlstr).fetchall()
     if not len(results):
@@ -120,22 +139,36 @@ def get_has_in_postgres_TF_IDF(lexem):
     # ndoc - Ответов с вхождением
     # nentry - Вхождений за всю базу
 
-    sqlstr = '''
+    if not only_questions:
+        sqlstr = '''
 
-        select distinct * from
-        (
-            SELECT 'questions' as col_name, 'A' as weight, 1.0 as weight_norm,  * FROM ts_stat('SELECT search_vector FROM telegram_bot_link_base  ', 'A')
-            union all
-            SELECT 'abstract' as icolumn, 'B' as  weight, 0.4 as weight_norm, * FROM ts_stat('SELECT search_vector FROM telegram_bot_link_base  ', 'B')
-            union all
-            SELECT 'keywords' as icolumn, 'C' as weight, 0.2 as weight_norm,  * FROM ts_stat('SELECT search_vector FROM telegram_bot_link_base  ', 'C')
-            union all
-            SELECT 'name' as icolumn, 'D' as weight, 0.1 as weight_norm,  * FROM ts_stat('SELECT search_vector FROM telegram_bot_link_base  ', 'D')
+            select distinct * from
+            (
+                SELECT 'questions' as col_name, 'A' as weight, 1.0 as weight_norm,  * FROM ts_stat('SELECT search_vector FROM telegram_bot_link_base  ', 'A')
+                union all
+                SELECT 'abstract' as icolumn, 'B' as  weight, 0.4 as weight_norm, * FROM ts_stat('SELECT search_vector FROM telegram_bot_link_base  ', 'B')
+                union all
+                SELECT 'keywords' as icolumn, 'C' as weight, 0.2 as weight_norm,  * FROM ts_stat('SELECT search_vector FROM telegram_bot_link_base  ', 'C')
+                union all
+                SELECT 'name' as icolumn, 'D' as weight, 0.1 as weight_norm,  * FROM ts_stat('SELECT search_vector FROM telegram_bot_link_base  ', 'D')
 
-        ) lemms where length(lemms.word)>1 and not isnumeric(lemms.word) and lemms.word='{}'
-        ORDER BY weight_norm DESC, nentry DESC, ndoc DESC, word
+            ) lemms where length(lemms.word)>1 and not isnumeric(lemms.word) and lemms.word='{lexem}'
+            ORDER BY weight_norm DESC, nentry DESC, ndoc DESC, word
 
-    '''.format(lexem)
+        '''.format(lexem=lexem)
+    else:
+        sqlstr = '''
+
+            select distinct * from
+            (
+                SELECT 'questions' as col_name, 'A' as weight, 1.0 as weight_norm,  * FROM ts_stat('SELECT q_search_vector FROM telegram_bot_link_base  ', 'A')
+                union all
+                SELECT 'keywords' as icolumn, 'B' as  weight, 0.4 as weight_norm, * FROM ts_stat('SELECT q_search_vector FROM telegram_bot_link_base  ', 'B')
+
+            ) lemms where length(lemms.word)>1 and not isnumeric(lemms.word) and lemms.word='{lexem}'
+            ORDER BY weight_norm DESC, nentry DESC, ndoc DESC, word
+
+        '''.format(lexem=lexem)
 
     results = session.execute(sqlstr).first()
     results = dict(results)
@@ -162,7 +195,7 @@ def main(main_window):
 
         if token['pg_lexem'] and len(synonyms):
 
-            pg_lexem, tf_idf = get_has_in_postgres_TF_IDF(token['pg_lexem'])
+            pg_lexem, tf_idf = get_has_in_postgres_TF_IDF(token['pg_lexem'], True)
 
             rupos = RUS_POS.get(token["POS"], token["POS"])
 
@@ -170,7 +203,7 @@ def main(main_window):
 
             has_tf_idf_tokens = False
             for synonym in synonyms:
-                pg_lexem_syn, tf_idf_syn = get_has_in_postgres_TF_IDF(synonym["pg_lexem"])
+                pg_lexem_syn, tf_idf_syn = get_has_in_postgres_TF_IDF(synonym["pg_lexem"], True)
                 if pg_lexem_syn:
                     allstr += f'    -{synonym["ann_lemma"]}<{synonym["pg_lexem"]}>, [tf_idf: {tf_idf_syn:.2f}][сходство: {synonym["weight"]:.2f}]\n'
                     has_tf_idf_tokens = True
@@ -186,4 +219,4 @@ def main(main_window):
 
 
 if __name__ == '__main__':
-    get_has_in_postgres_TF_IDF('поступ')
+    get_has_in_postgres_TF_IDF('поступ', True)
