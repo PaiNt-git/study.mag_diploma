dd1c7a2cbdbfca27e8714166d535cc4860714a61
PaiNt <PaiNt@ROSHPIT>
2024-05-07 10:12:35 +1000

    fix

--- src/info_service/actions/win_relev_metric_refresh.py
+++ src/info_service/actions/win_relev_metric_refresh.py
@@ -13,9 +13,28 @@ from collections import OrderedDict
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+from info_service.db_base import Session, QuestAnswerBase, QuestAnswerBaseRelevQuery, QuestAnswerBaseRelevQueryRel
+from info_service.db_utils import togudb_serializator
+
 
 def main(main_window):
     from info_service import actions
+
+    only_questions = main_window.MAINWINDOW_LOCAL_STORAGE['only_questions']
+
+    session = Session()
+
+    K = 10
+
+    all_queries = session.query(QuestAnswerBaseRelevQuery)
+    count_queries = all_queries.count()
+
+    try:
+        p_at_K = '[' + ', '.join([f'{actions.metrics_getPrecisionAtK(x.query, K, only_questions)}' for x in all_queries]) + ']'
+        ap_at_K = '[' + ', '.join([f'{actions.metrics_getAvgPrecisionOfKres(x.query, K, only_questions)}' for x in all_queries]) + ']'
+        map_at_K = f'{actions.metrics_getPrecisionAtK(K, only_questions)}'
+    except Exception as e:
+        print(e)
 
     enreturn = '''
 <!DOCTYPE HTML>
@@ -39,9 +58,23 @@ def main(main_window):
 
 </head><body class="zoomed">
 
-$$x = {-b \pm \sqrt{b^2-4ac} \over 2a}$$
+<h4>Precision at K (p@K)</h4>
+<div>Tочность на K элементах — базовая метрика качества ранжирования для одного объекта.</div>
+$$p@K = \frac{\sum_{k=1}^{K}{r^{true}(\pi^{-1})(k))}}{K} = \frac{релевантных элементов}{K} = ''' + p_at_K + '''$$
 
-$$x = {-b \pm \sqrt{b^2-4ac} \over 2a}.$$
+<h4>Average precision at K (ap@K)</h4>
+<div>Равна сумме p@k по индексам k от 1 до K только для релевантных элементов, деленому на K</div>
+$$ap@K = \frac{1}{K}\sum_{k=1}^{K}{r^{true}(\pi^{-1})(k))}\cdot p@k = ''' + ap_at_K + '''$$
+
+<h4>Mean average precision at K (map@K)</h4>
+<div>одна из наиболее часто используемых метрик качества ранжирования.
+В p@K и ap@K качество ранжирования оценивается для отдельно взятого объекта (пользователя, поискового запроса).
+На практике объектов множество: мы имеем дело с сотнями тысяч пользователей, миллионами поисковых запросов и т.д. Идея map@K заключается в том, чтобы посчитать ap@K для каждого объекта и усреднить</div>
+$$map@K = \frac{1}{N}\sum_{j=1}^{N}{ap@K_{j}} = ''' + map_at_K + '''$$
+
+
+<i>-, где $$K = ''' + K + '''$$, N  запросов $$ U = \left\{ u_{i} \right\}_{i=1}^{N} $$ M ответов в выдаче $$ E = \left\{ e_{j} \right\}_{j=1}^{M} $$. </i>
+
 
 </body></html>
 '''
