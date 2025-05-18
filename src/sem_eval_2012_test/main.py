65ee918eee28c72c524d0d34a89c8384d2ed4c69
PaiNt <PaiNt@192.168.1.12>
2025-05-18 10:26:54 +1000

    fix

--- src/sem_eval_2012_test/main.py
+++ src/sem_eval_2012_test/main.py
@@ -16,6 +16,8 @@ import timeit
 import math
 import pandas
 
+from math import nan
+
 from googletrans import Translator
 
 from dataclasses import make_dataclass
@@ -28,7 +30,12 @@ from scipy.stats import spearmanr
 from sklearn.metrics.pairwise import cosine_similarity
 
 from itertools import groupby
-from math import nan
+
+
+from matplotlib import patches
+from scipy.spatial import ConvexHull
+import warnings; warnings.simplefilter('ignore')
+import time
 
 
 # https://sites.google.com/site/semeval2012task2/home?authuser=0
@@ -89,6 +96,7 @@ if __name__ == '__main__':
             pickle.dump(test_pairs, filep)
 
     MODEL_LIST = [
+        'etalon',
         'test_models/4corpora_3,5Msentences.gz.bin',
         'test_models/navec_news_v1_1B_250K_300d_100q.tar',
         'test_models/navec_hudlit_v1_12B_500K_300d_100q.tar',
@@ -99,53 +107,83 @@ if __name__ == '__main__':
     max_Spearman_allcount = {}
     max_Spearman_oov = {}
 
-    for model_name in MODEL_LIST:
+    import matplotlib.pyplot as plt
 
-        if not 'navec_' in model_name:
-            gensim_model = gensim.models.Word2Vec.load(model_name)
-        else:
-            navec_model = ModifNavec.load(model_name)
-            gensim_model = navec_model.as_gensim
+    # As many colors as there are unique midwest['category']
+    categories = [os.path.basename(x) for x in MODEL_LIST]
+    colors = [plt.cm.tab10((i / float(len(categories) - 1))) for i in range(len(categories))]
+    colors_fg = [plt.cm.tab20(i / float(len(categories) - 1)) for i in range(len(categories))]
 
-        kv_model = gensim_model.wv if type(gensim_model).__name__ == 'Word2Vec' else gensim_model
+    # Step 2: Draw Scatterplot with unique color for each category
+    fig = plt.figure(figsize=(16, 10), dpi=80, facecolor='w', edgecolor='k')
+
+    # Step 3: Encircling
+    # https://stackoverflow.com/questions/44575681/how-do-i-encircle-different-data-sets-in-scatter-plot
+    def encircle(x, y, ax=None, **kw):
+        if not ax: ax = plt.gca()
+        p = np.c_[x, y]
+        hull = ConvexHull(p)
+        poly = plt.Polygon(p[hull.vertices, :], **kw)
+        ax.add_patch(poly)
+
+    size_legend = None
+
+    for model_i, model_name in enumerate(MODEL_LIST):
+
+        if model_name != 'etalon':
+            if not 'navec_' in model_name:
+                gensim_model = gensim.models.Word2Vec.load(model_name)
+            else:
+                navec_model = ModifNavec.load(model_name)
+                gensim_model = navec_model.as_gensim
+
+            kv_model = gensim_model.wv if type(gensim_model).__name__ == 'Word2Vec' else gensim_model
+
+        model_name_ = os.path.basename(model_name)
 
         for filestr, tpairs_ in groupby(test_pairs, key=lambda x: x['filestr']):
-
-            # Прогнозирование
             human_scores = []
             model_scores = []
             tpairs = list(tpairs_)
             oov_count = 0
 
-            for semitem in tpairs:
-                word1 = semitem['pair_rus'][0]
-                word2 = semitem['pair_rus'][1]
-                score = semitem['score']
+            if model_name != 'etalon':
+                # Прогнозирование
+                for semitem in tpairs:
+                    word1 = semitem['pair_rus'][0]
+                    word2 = semitem['pair_rus'][1]
+                    score = semitem['score']
 
-                if word1 in kv_model and word2 in kv_model:
-                    emb1 = kv_model[word1].reshape(1, -1)
-                    emb2 = kv_model[word2].reshape(1, -1)
-                    sim = cosine_similarity(emb1, emb2)[0][0]
-                    human_scores.append(score)
-                    model_scores.append(sim)
-                else:
-                    oov_count += 1
+                    if word1 in kv_model and word2 in kv_model:
+                        emb1 = kv_model[word1].reshape(1, -1)
+                        emb2 = kv_model[word2].reshape(1, -1)
 
-            human_scores = np.array(human_scores)
-            model_scores = np.interp(model_scores, (-1, 1), (-100, 100))
+                        sim = cosine_similarity(emb1, emb2)[0][0]
+                        human_scores.append(score)
+                        model_scores.append(sim)
+
+                    else:
+                        oov_count += 1
+
+                human_scores = np.array(human_scores)
+                model_scores = np.interp(model_scores, (-1, 1), (-100, 100))
 
             # Оценка
-            rho, _ = spearmanr(human_scores, model_scores)
+            if model_name != 'etalon':
+                rho, _ = spearmanr(human_scores, model_scores)
+            else:
+                rho = float(Spearman_etalon.get(filestr, 0.0))
 
             allcount = len(tpairs)
-            if not math.isnan(rho) and ((oov_count / allcount) if allcount else 1.0) < 0.80:
-                model_name_ = os.path.basename(model_name)
+            if not math.isnan(rho) and ((oov_count / allcount) if allcount else 1.0) < 0.75:
                 results[model_name_].append({'Spearman_etalon_val': tpairs[0]['Spearman_etalon_val'],
                                              'Spearman': rho,
                                              'filestr': filestr,
                                              'oov_count': oov_count,
                                              'allcount': allcount,
-                                             'weight': (1 / (oov_count / allcount)) if oov_count else 1.0
+                                             'weight': 1 - (oov_count / allcount),
+                                             'dot_size': 200 * ((1 - (oov_count / allcount))),
+                                             'modelname': model_name_,
                                              })
                 print(f"Модель:{model_name_}, Корреляция Спирмена: {rho:.3f}")
                 if not model_name_ in max_Spearman or max_Spearman[model_name_] < rho:
@@ -153,7 +191,7 @@ if __name__ == '__main__':
                     max_Spearman[model_name_] = rho
                     max_Spearman_allcount[model_name_] = allcount
             else:
-                print(f"Все oov или больше 80%")
+                print(f"Все oov или больше 75%")
 
         df = pandas.DataFrame.from_dict(results[model_name_])
 
@@ -164,5 +202,58 @@ if __name__ == '__main__':
 
 
         ''')
+
+        plotname = f'{model_name_}'
+        if model_name_ == 'etalon':
+            plotname = 'Эталонная кореляция типичности "SemEval-2012"'
+
+        sc = plt.scatter(x=df.index.array, y='Spearman', data=df, s='dot_size', c=colors[model_i], label=str(plotname), edgecolors='black', linewidths=.5)
+
+        def g(s): return s / 200
+        size_legend = plt.legend(*sc.legend_elements("sizes", num=4, func=g), frameon=False, title="Качество по OOV", loc='upper right')
+
+        # Select data to be encircled
+        midwest_encircle_data = df
+
+        # Draw polygon surrounding vertices
+        encircle(midwest_encircle_data.index.array, midwest_encircle_data.Spearman, ec="none", fc=colors_fg[model_i], alpha=0.05)
+        encircle(midwest_encircle_data.index.array, midwest_encircle_data.Spearman, ec=colors[model_i], fc="none", linewidth=0.5, linestyle=':')
+
+    # Step 4: Decorations
+    plt.gca().set(ylim=(-0.75, 1.35), xlabel='Группы тестов', ylabel='Коэффицент корреляции Спирмана: ρ')
+
+    plt.gca().xaxis.label.set(fontsize=16)
+    plt.gca().yaxis.label.set(fontsize=16)
+
+    plt.axhline(y=0.7,
+                color='green',
+                linestyle='--',
+                linewidth=0.75,
+                label=f'Хорошее качество (близко к человеческим оценкам)')
+
+    plt.axhline(y=0.0,
+                color='red',
+                linewidth=1.5,
+                label=f'Корреляция меньше нуля')
+
+    plt.xticks([]); plt.yticks(fontsize=12)
+
+    if len(MODEL_LIST) > 2:
+        windtitle = f'''
+    Тесты семантической близости "SemEval-2012-Platinum-Ratings"
+    ({len(MODEL_LIST)-1} модели)
+    '''
+    else:
+        windtitle = f'''
+    Тесты семантической близости "SemEval-2012-Platinum-Ratings"
+    (Модель "{os.path.basename(MODEL_LIST[1])}")
+    '''
+
+    plt.title(windtitle, fontsize=20)
+
+    plt.legend(fontsize=12, loc='lower right')
+    plt.gca().add_artist(size_legend)
+
+    plt.show()
 
     pass
